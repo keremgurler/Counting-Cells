495b874c5377d653d2ceb71efc2f6a4f0783e4f6
keremgurler <kerem.gurler@hotmail.com>
2019-05-30 21:35:41 +0300

    Queries

--- Interface.java
+++ Interface.java
@@ -6,10 +6,15 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
+
+import net.proteanit.sql.DbUtils;
+
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.RenderedImage;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -22,6 +27,18 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
@@ -41,16 +58,32 @@ public class Interface extends JFrame {
 	private static Object[][] columnData;
 	private JPanel contentPane;
 	private JPanel panel;
-	private JPanel panel_1;
-
+	private static JTable table;
+	private final static String root = System.getProperty("user.home");
+	private static Connection connection = sqliteConnection.dbConnector();
+	private JTextField textField;
 	/**
 	 * Launch the application.
 	 * 
 	 * @throws IOException
+	 * @throws SQLException 
 	 */
-	public static void main(String[] args) throws IOException  {
-		readHistory();
-		
+	public static void refreshTable() {
+		try {
+			String query = "SELECT rowid, date,name,number FROM CellCount ORDER BY date";
+			PreparedStatement prt = connection.prepareStatement(query);
+			ResultSet rs = prt.executeQuery();
+			table.setModel(DbUtils.resultSetToTableModel(rs));
+			prt.close();
+			rs.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	public static void main(String[] args) throws IOException, SQLException  {	
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -63,42 +96,14 @@ public class Interface extends JFrame {
 		});
 	}
 	
-	public static void readHistory() throws IOException {
-		File historyFile = new File(System.getProperty("user.home") + "\\history.csv");
 
-		try {
-			historyFile.createNewFile();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try {
-
-			BufferedReader reader = new BufferedReader(new FileReader(historyFile));
-			String line = reader.readLine();
-			Path path = Paths.get(System.getProperty("user.home") + "\\history.csv");
-			long lineCount = Files.lines(path).count();
-			columnData = new Object[(int) lineCount][2];
-			int i = 0;
-			while (line != null) {
-
-				columnData[i][0] = line.split(",")[0];
-				columnData[i][1] = Integer.parseInt(line.split(",")[1]);
-				line = reader.readLine();
-				i++;
-			}
-		} catch (FileNotFoundException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		}
-	}
 
 	/**
 	 * Create the frame.
 	 */
 	public Interface() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1056, 522);
+		setBounds(100, 100, 722, 522);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -109,19 +114,9 @@ public class Interface extends JFrame {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		buttonDisplay.setBounds(135, 41, 114, 35);
-		contentPane.add(buttonDisplay);
-		String[] listLengthCount = { "Length", "Count" };
 		
-		JComboBox lengthCountSelection = new JComboBox(listLengthCount);
-		lengthCountSelection.setBounds(747, 41, 114, 35);
-		contentPane.add(lengthCountSelection);
-
-		JLabel lblLengthOrCount = new JLabel("Length or Count");
-		lblLengthOrCount.setBounds(605, 47, 97, 23);
-		contentPane.add(lblLengthOrCount);
-
-		String[] HistoryList = { "1st experiment", "2nd experiment", "3rd experiment", "4th experiment" };
+		buttonDisplay.setBounds(104, 40, 114, 35);
+		contentPane.add(buttonDisplay);
 
 		panel = new JPanel();
 		panel.setBounds(65, 124, 423, 311);
@@ -135,34 +130,89 @@ public class Interface extends JFrame {
 
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(12, 42, 400, 256);
+		
+		scrollPane.getViewport();
 		panel.add(scrollPane);
 
-		String[] columnNames = { "Date", "Count" };
-		JTable workHistory = new JTable(columnData, columnNames);
-		scrollPane.setViewportView(workHistory);
+		table = new JTable();
 
-		panel_1 = new JPanel();
-		panel_1.setBounds(563, 124, 423, 311);
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
+		
+		
+		
+		int col=table.getSelectedColumn();
+		int row=table.getSelectedRow();
+		//do the update query on this row
+		
+		
+		
+		scrollPane.setViewportView(table);
+		refreshTable();
+		
+		//String[] microscopeTypes = {"optic", "fluoroscent", "TEM"};
+		
+		JButton btnSaveChanges = new JButton("Save Changes");
+		btnSaveChanges.setBounds(515, 40, 134, 35);
+		btnSaveChanges.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int col=table.getSelectedColumn();
+				int row=table.getSelectedRow();
+				
+				//do the update query on this row
+				  
+				try {
+				           PreparedStatement ps = connection.prepareStatement("UPDATE CellCount SET number = " + textField.getText() + " WHERE rowid = "+(row+1));
 
-		Canvas canvas = new Canvas();
-		canvas.setBounds(10, 10, 276, 198);
-		panel_1.add(canvas);
+				            ps.executeUpdate(); 
+				            ps.close();
+				            refreshTable();
+				           
+				        } catch (Exception ex) {
+				          ex.printStackTrace();
+				        }
+			}
+		});
+		contentPane.add(btnSaveChanges);
 		
-		String[] microscopeTypes = {"optic", "fluoroscent", "TEM"};
-		JComboBox microscopeSelection = new JComboBox(microscopeTypes);
-		microscopeSelection.setBounds(424, 41, 107, 35);
-		contentPane.add(microscopeSelection);
+		textField = new JTextField();
+		textField.setBounds(513, 154, 116, 22);
+		contentPane.add(textField);
+		textField.setColumns(10);
 		
-		JLabel lblMicroscopeType = new JLabel("Microscope Type");
-		lblMicroscopeType.setBounds(305, 44, 107, 29);
-		contentPane.add(lblMicroscopeType);
+		JButton btnCreateGraph = new JButton("Create Graph");
+		btnCreateGraph.setBounds(290, 40, 129, 35);
+		contentPane.add(btnCreateGraph);
+		btnCreateGraph.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				int[] indexes = table.getSelectedRows();
+				System.out.println(indexes[0] + "\n" + indexes[1]);
+				List<Double> graphValues = new ArrayList();
+				for(int i = 0; i < indexes.length; i++) {
+					String graphQuery = "SELECT number FROM CellCount WHERE rowid= " + (indexes[i] + 1);
+					try {
+						PreparedStatement ps = connection.prepareStatement(graphQuery);
+						ResultSet rs = ps.executeQuery();
+						while(rs.next()) {
+							graphValues.add(rs.getDouble(1));
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				GraphPanel gp = new GraphPanel(graphValues);
+				gp.createAndShowGui(graphValues);
+				
+			}
+			
+		});
 
 		buttonDisplay.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
-
+				
 				JFileChooser file = new JFileChooser();
 				file.setCurrentDirectory(new File(System.getProperty("user.home")));
 				// filter the files
@@ -199,12 +249,45 @@ public class Interface extends JFrame {
 				        	ProcessBuilder pb = new ProcessBuilder("C:\\Python27\\python.exe", "C:\\Python27\\LengthAndCount.py",path, path.concat("_out.jpg"));
 							Process p = pb.start();
 							
+							Pattern pattern = Pattern.compile(".*\\\\s*(.*)");
+							Matcher matcher = pattern.matcher(path);
+							System.out.println();
+							String fileName = "";
+							if (matcher.find())
+								fileName = matcher.group(1);
+							String insertQuery = "INSERT INTO CellCount (date, name, number) VALUES (?,?,?)";
+
+						
 							
 							BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
 							String s = null;
-							while ((s= in.readLine())!= null )
+							int cellCount  = -1;
+							while((s= in.readLine())!= null) {
 								System.out.println(s);
-				        	
+								cellCount++;
+							}
+							
+							FileWriter writer = new FileWriter(root + "\\Documents\\CountingCells\\history.txt", true);
+							DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+							Date date = new Date();
+							String dateString = date.toString();
+							try {
+								PreparedStatement prt = connection.prepareStatement(insertQuery);
+								prt.setString(1, dateString);
+								prt.setString(2, fileName);
+								prt.setString(3, Integer.toString(cellCount));
+								prt.execute();
+								prt.close();
+							}
+							catch(Exception k){
+								k.printStackTrace();
+							}
+							
+							
+							writer.write(dateFormat.format(date) + "," + cellCount + "\n");
+							writer.flush();
+							System.out.println(cellCount);
+							refreshTable();
 				        }
 
 					} catch (IOException e1) {
